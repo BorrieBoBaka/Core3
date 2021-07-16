16db7b2b6d35acd820731a677b913636d1a9db91
BorrieBoBaka <borriebobaka@gmail.com>
2021-07-16 14:29:23 -0400

    [Development] Further work on the Vendor system

--- MMOCoreORB/bin/scripts/screenplays/borrie/conv_handlers/rpg_shopkeeper_convo_handler.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/conv_handlers/rpg_shopkeeper_convo_handler.lua
@@ -37,7 +37,7 @@ function rpg_shopkeeper_convo_handler:getNextConversationScreen(conversationTemp
         if(string.find(optionData, "$vnd")) then
 			local results = HelperFuncs:splitString(optionData, ":")
 			if(results[2] == "item") then
-				CreatureObject(conversingPlayer):sendSystemMessage(results[3])
+				--CreatureObject(conversingPlayer):sendSystemMessage(results[3])
 				
 				local pGhost = CreatureObject(conversingPlayer):getPlayerObject()
 
@@ -46,20 +46,58 @@ function rpg_shopkeeper_convo_handler:getNextConversationScreen(conversationTemp
 				end
 				CreatureObject(conversingPlayer):
 				local suiManager = LuaSuiManager()
-				local itemName = getItemTemplateName(results[4])
+				--local itemName = getItemTemplateName(results[4])
 				local itemDescription = getItemTemplateInformation(results[4])
+
+                SceneObject(conversingPlayer):setStoredInt("vendor:itemCost", results[3])
+                SceneObject(conversingPlayer):setStoredString("vendor:itemTemplate", results[4])
 				
 				suiManager:sendMessageBox(conversingPlayer, conversingPlayer, "Merchandise Info", itemDescription, "@confirm", "rpg_shopkeeper_convo_handler", "notifyPurchaseItem", NEWSNET_INFO)
 				
-			end
+			elseif(results[2] == "category") then
+                SceneObject(conversingPlayer):setStoredInt("vendor:category", results[3])
+            end
         end
 	end
 
     return nextConversationScreen
 end
 
-function rpg_shopkeeper_convo_handler:notifyPurchaseItem() 
-	
+function rpg_shopkeeper_convo_handler:notifyPurchaseItem(pPlayer, pSui, eventIndex, arg0) 
+    local cancelPressed = (eventIndex == 1)
+
+	if (pPlayer == nil or pSui == nil or cancelPressed) then
+		return
+	end
+
+    local pGhost = CreatureObject(pPlayer):getPlayerObject()
+
+	if (pGhost == nil) then
+		return
+	end
+
+	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
+
+	if (pInventory == nil) then
+		return
+	end
+
+    local template =  SceneObject(pPlayer):getStoredString("vendor:itemTemplate")
+    local cost =  SceneObject(pPlayer):getStoredInt("vendor:itemCost")
+    local itemName = getItemTemplateName(template)
+
+    if (CreatureObject(pPlayer):getCashCredits() < cost) then
+		CreatureObject(pPlayer):sendSystemMessage("You do not have enough credits to buy that.")
+		return
+    elseif (SceneObject(pInventory):isContainerFullRecursive()) then
+        CreatureObject(pPlayer):sendSystemMessage("You cannot buy this. Your inventory is full.")
+        return
+    end
+
+    CreatureObject(pPlayer):subtractCashCredits(cost)
+
+	local pItem = giveItem(pInventory, template, -1)
+    CreatureObject(pPlayer):sendSystemMessage("You purchased \"" .. itemName .. "\" for " .. cost .. " credits.")
 end
 
 function rpg_shopkeeper_convo_handler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen)
@@ -97,17 +135,18 @@ function rpg_shopkeeper_convo_handler:runScreenHandlers(conversationTemplate, co
     if(screenID == "browse") then
         --We want to get all the categories available
         clonedConversation:removeAllOptions()
-        for i = 1, #vendorList.categories, 1 do
-            clonedConversation:addOptionWithData("$vnd:category:" .. i, vendorList.categories[i], "items")
+        for i = 1, #vendorList.manifest, 1 do
+            clonedConversation:addOptionWithData("$vnd:category:" .. i, vendorList.manifest[i].name, "items")
         end
         clonedConversation:addOption("Nevermind.", "abort")
     elseif(screenID == "items") then
         clonedConversation:setCustomDialogText(vendorTheme.dialog.items)
         clonedConversation:removeAllOptions()
-        for i = 1, #vendorList.manifest["Pistols"].items, 1 do
-            local templateName = vendorList.manifest["Pistols"].items[i].template
+        local selectedCategory = SceneObject(conversingPlayer):getStoredInt("vendor:category")
+        for i = 1, #vendorList.manifest[selectedCategory].items, 1 do
+            local templateName = vendorList.manifest[selectedCategory].items[i].template
             local objectName = getItemTemplateName(templateName)
-            local price = vendorList.manifest["Pistols"].items[i].cost;
+            local price = vendorList.manifest[selectedCategory].items[i].cost
             clonedConversation:addOptionWithData("$vnd:item:" .. price .. ":" .. templateName, objectName, "items")
         end
         clonedConversation:addOption("I'd rather look at something else.", "browse")		
--- MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_general.lua
+++ MMOCoreORB/bin/scripts/screenplays/borrie/shop_lists/rpg_shop_general.lua
@@ -1,15 +1,16 @@
 rpg_shop_general = {
-	categories = {"Pistols", "Medicine"},
 	manifest = {
-		["Pistols"] = {
-			minItems = 1;
+		{
+			name = "Pistols",
+			minItems = 1,
 			items = {
 				{template = "object/weapon/ranged/pistol/pistol_dl44.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/weapon/ranged/pistol/pistol_fwg5.iff", cost = 250, max = 10, chance = 100 },
 			}
 		},
-		["Armor"] = {
-			minItems = 2;
+		{
+			name = "Armor",
+			minItems = 2,
 			items = {
 				{template = "object/tangible/wearables/armor/chitin/armor_chitin_s01_helmet.iff", cost = 250, max = 10, chance = 100 },
 				{template = "object/tangible/wearables/armor/composite/armor_composite_chest_plate.iff", cost = 250, max = 10, chance = 100 },
